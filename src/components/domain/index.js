f6b71ba9f8a4465d6fa68a0daa5a2a0aef3accb0
VerdaMech <verdaguergvice@gmail.com>
2026-05-05 22:09:30 -0400

    feat(mapa): agregar componente MapaTiendas con pines y geolocalización

--- src/components/domain/index.js
+++ src/components/domain/index.js
@@ -9,3 +9,5 @@ export { DeckBuilder } from './DeckBuilder';
 export { default as RoleBadge } from './RoleBadge';
 export { default as EstadisticasJugador } from './EstadisticasJugador';
 export { default as MiniMapaTienda } from './MiniMapaTienda';
+export { default as MapaTiendas } from './MapaTiendas';
+export { crearStorePinIcon } from './StorePin';
